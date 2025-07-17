cafd58c45120e313f69f0ea4bb5cd04f96b1dc10
SKINPALL <max.095214@gmail.com>
2025-07-17 22:44:00 +0700

    feat: Menu bar
    Add menu bar with html, css

--- src/component/MenuBar.jsx
+++ src/component/MenuBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
+
 function MenuBar() {
+
     return (
         <div className="menu-container">
 
@@ -8,15 +10,53 @@ function MenuBar() {
 
                 <div className="menu-buddyplan">
 
-                    
+
                     <img src="src/component/buddyplan-logo.png" alt="buddyplanner-logo" height={50} />
-                    <p>Buddy<br />Planner </p>
+                    <p style={{ fontWeight: "bold", color: "#7DA2BC" }}>Buddy<br /><span style={{ color: "#88706F" }}>Planner</span> </p>
 
                 </div>
             </div>
-            <div className="menu-right"></div>
+            <div className="menu-right">
 
-        </div>
+                <div className="home">
+
+                    <img src="src/component/logo/home-logo.png" alt="home-logo" height={24} />
+                    <a href="#">HOME</a>
+
+                </div>
+
+                <div className="my-planner">
+
+                    <img src="src/component/logo/myPlanner-logo.png" alt="myPlanner-logo" height={24} />
+                    <a href="#">MY PLANNER</a>
+
+                </div>
+
+                <div className="goal">
+
+                    <img src="src/component/logo/goal-logo.png" alt="goal-logo" height={24} />
+                    <a href="#">GOALS</a>
+
+                </div>
+
+                <div className="history">
+
+                    <img src="src/component/logo/history-logo.png" alt="history-logo" height={24} />
+                    <a href="#">HISTORY</a>
+
+                </div>
+
+
+                <div className="contact">
+
+                    <img src="src/component/logo/contact-logo.png" alt="contact-logo" height={24} />
+                    <a href="#">CONTACT</a>
+
+                </div>
+
+            </div>
+
+        </div >
     )
 }
 
