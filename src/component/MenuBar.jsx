cbbb923950de0de5f06a2e35afa739428cc9ed90
SKINPALL <max.095214@gmail.com>
2025-07-17 21:03:19 +0700

    first commit

---
+++ src/component/MenuBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function MenuBar() {
+    return (
+        <div className="menu-container">
+
+            <div className="menu-left">
+
+                <div className="menu-buddyplan">
+
+                    
+                    <img src="src/component/buddyplan-logo.png" alt="buddyplanner-logo" height={50} />
+                    <p>Buddy<br />Planner </p>
+
+                </div>
+            </div>
+            <div className="menu-right"></div>
+
+        </div>
+    )
+}
+
+export default MenuBar
